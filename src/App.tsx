eba0c2b1eb9545f726116349b6059c195e349bf5
Scott Clingan <sclingan1@gmail.com>
2023-05-09 14:04:40 -0500

    Adding basic desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import logo from '../public/images/logo.svg'
 import mockup from '../public/images/illustration-mockups.svg'
+import facebook from '../public/images/icon-facebook.svg'
+import twitter from '../public/images/icon-twitter.svg'
 import './App.css'
 
 function App() {
@@ -7,8 +9,9 @@ function App() {
   return (
     <div className='container'>
       <header>
+        {/* <h1 className='sr-only'>Huddle landing page</h1> */}
          <img className='logo' src={logo} alt=""></img>
-         <img src={mockup} alt=""></img>
+         <img className='mockup' src={mockup} alt=""></img>
       </header>
       <main>
         <h2>Build The Community Your Fans Will Love</h2>
@@ -16,11 +19,12 @@ function App() {
           Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
           Create connections with your users as you engage in genuine discussion. 
         </p>
-        <a href="">Register</a>
+        <a className="register" href="">Register</a>
         </main>
         <footer>
          <nav>
-
+            <a href=""><img src={facebook} alt=""></img></a>
+            <a href=""><img src={twitter} alt=""></img></a>
          </nav>
         </footer>
     </div>
