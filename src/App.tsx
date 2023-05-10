4ae1fc749e0d5a148c845618bbceb3d3687a496f
Scott Clingan <sclingan1@gmail.com>
2023-05-10 14:13:41 -0500

    Added nav and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import logo from '../public/images/logo.svg'
 import mockup from '../public/images/illustration-mockups.svg'
-import facebook from '../public/images/icon-facebook.svg'
-import twitter from '../public/images/icon-twitter.svg'
 import './App.css'
 
 function App() {
@@ -23,8 +21,15 @@ function App() {
         </main>
         <footer>
          <nav>
-            <a href=""><img src={facebook} alt=""></img></a>
-            <a href=""><img src={twitter} alt=""></img></a>
+            <a href="">
+              <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><path fill="#fff" d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189 221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15 4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31z" fillRule="evenodd"/></svg>
+            </a>
+            <a href="">
+              <svg xmlns="http://www.w3.org/2000/svg" className="ionicon twitter" width="20" height="20"><path fill="#FFF" d="M20 2.172a8.192 8.192 0 01-2.357.646 4.11 4.11 0 001.804-2.27 8.22 8.22 0 01-2.605.996A4.096 4.096 0 0013.847.248c-2.65 0-4.596 2.472-3.998 5.037A11.648 11.648 0 011.392 1a4.109 4.109 0 001.27 5.478 4.086 4.086 0 01-1.858-.513c-.045 1.9 1.318 3.679 3.291 4.075a4.113 4.113 0 01-1.853.07 4.106 4.106 0 003.833 2.849A8.25 8.25 0 010 14.658a11.616 11.616 0 006.29 1.843c7.618 0 11.922-6.434 11.663-12.205A8.354 8.354 0 0020 2.172z"/></svg>
+            </a>
+            <a href="">
+              <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><path fill="#FFF" d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z"/><path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z"/></svg>
+            </a>
          </nav>
         </footer>
     </div>
